9e4f9daa7cc3cf67df8196b771a5ff2cb627f208
capital.phyoethurakyaw@gmail.com <capital.phyoethurakyaw@gmail.com>
2022-12-12 03:23:46 +0630

    PTK

--- Crawler/Form1.Designer.cs
+++ Crawler/Form1.Designer.cs
@@ -29,78 +29,161 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtDriver = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1156, 12);
+            this.button1.Location = new System.Drawing.Point(600, 105);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(109, 50);
+            this.button1.Size = new System.Drawing.Size(109, 30);
             this.button1.TabIndex = 0;
             this.button1.Text = "Run";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // webBrowser1
-            // 
-            this.webBrowser1.Location = new System.Drawing.Point(12, 12);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(456, 589);
-            this.webBrowser1.TabIndex = 1;
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(1156, 189);
+            this.button2.Location = new System.Drawing.Point(600, 157);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(109, 50);
+            this.button2.Size = new System.Drawing.Size(109, 32);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Export";
+            this.button2.Text = "Open";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(526, 28);
+            this.textBox1.Location = new System.Drawing.Point(174, 111);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(586, 20);
+            this.textBox1.Size = new System.Drawing.Size(310, 20);
             this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "https://www.chotot.com/";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1054, 69);
+            this.label1.Location = new System.Drawing.Point(59, 255);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(34, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "Total-";
             // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(171, 255);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(13, 13);
+            this.lblCount.TabIndex = 5;
+            this.lblCount.Text = "0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(174, 211);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(160, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Initial URL";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Specify Total Number";
+            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(1092, 69);
+            this.label2.Location = new System.Drawing.Point(59, 167);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(13, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "0";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Data";
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(174, 164);
+            this.txtData.Name = "txtData";
+            this.txtData.ReadOnly = true;
+            this.txtData.Size = new System.Drawing.Size(310, 20);
+            this.txtData.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(600, 280);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 30);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Browse . . .";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtDriver
+            // 
+            this.txtDriver.Location = new System.Drawing.Point(174, 286);
+            this.txtDriver.Name = "txtDriver";
+            this.txtDriver.ReadOnly = true;
+            this.txtDriver.Size = new System.Drawing.Size(310, 20);
+            this.txtDriver.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 289);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Chrome Driver Path";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1287, 619);
+            this.ClientSize = new System.Drawing.Size(801, 579);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDriver);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblCount);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -110,11 +193,19 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtDriver;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- Crawler/Form1.cs
+++ Crawler/Form1.cs
@@ -27,21 +27,86 @@ namespace Crawler
         }
         string pth = @"C:\chotot\ChototPhoneNumber.txt";
         string DriverPath= @"C:\Program Files\Google\Chrome\Application\";
-        int MaxTimes = 50;
+        int MaxTimes =  0;
         List<string> lst = new List<string>();
         bool Is50000 = false;
+        ChromeDriver cdr;
         private void button1_Click(object sender, EventArgs e)
         {
-           lst= File.ReadAllLines(pth).ToList<string>();
-           // label2.Text = lst.Count.ToString();
-            var driverC = new ChromeDriver(DriverPath); 
-            while (!Is50000)
+         
+            Thread thread = new Thread(new ThreadStart(Start));
+            thread.Start();
+         
+        }
+        private void CheckStartDamain(ChromeDriver driverC)
+        {
+            driverC.Navigate().GoToUrl(textBox1.Text);
+
+            if (textBox1.Text.ToLower().ToString().Contains("https://www.chotot.com"))
             {
-                label2.Text = lst.Count.ToString();
-                RunSele(driverC, textBox1.Text);
+                var lstelem = driverC.FindElements(By.TagName("a"));
+
+               // var lstelem = driverC.FindElements(By.TagName("a"));
+                Thread.Sleep(2000);
+                if (lstelem.Count > 0 )
+                {
+                    foreach (var anchr in lstelem)
+                    {
+                        try
+                        {
+
+                            var an = anchr.GetAttribute("href");
+                            if (an != null)
+                                if (an.ToLower().ToString().Contains("htm") && an.ToLower().ToString().Contains(".com"))
+                                {
+                                    driverC.Navigate().GoToUrl(an.TrimEnd());
+                                    Thread.Sleep(2000);
+                                    break;
+                                }
+                        }
+                        catch { }
+                    }
+                }
             }
-            driverC.Quit();
-            this.Close();
+        }
+        private void Start()
+        {
+            if (String.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text) || !Int64.TryParse(textBox2.Text.Trim(), out long result) || String.IsNullOrEmpty(txtDriver.Text))
+            {
+                MessageBox.Show("Please fill the required correct value");
+                return;
+            }
+
+            MaxTimes = Convert.ToInt32(textBox2.Text.Trim());
+
+            //lst = File.ReadAllLines(pth).ToList<string>();
+            // label2.Text = lst.Count.ToString();
+            cdr = new ChromeDriver(txtDriver.Text);
+            if (!textBox1.Text.Contains("htm"))
+            {
+                CheckStartDamain(cdr);
+                Thread.Sleep(3000);
+                textBox1.Text = cdr.Url;
+            }
+            while (!Is50000)
+            { 
+                if (lst.Count > MaxTimes || Is50000)
+                {
+                    MessageBox.Show("All "+ textBox2.Text +" Phone Numbers were Retrieved Successfully!");
+                    break;
+                }
+                //textBox1.Text = driverC;
+                try
+                {
+                    RunSele(cdr, textBox1.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            cdr.Quit();
+            MessageBox.Show("All " + textBox2.Text + " Phone Numbers were Retrieved Successfully!"); 
         }
         private void RunSele(ChromeDriver driverC, string url)
         {
@@ -50,10 +115,9 @@ namespace Crawler
                 driverC.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
                 driverC.Manage().Window.Maximize();
 
-                //  var url = "https://xe.chotot.com/mua-ban-xe-tai-xe-ben-huyen-quoc-oai-ha-noi/101909944.htm?ctfp=3c15e79f-de73-41fa-ae40-4d7c8bb14ae7&_gl=1*1l0mli7*_ga*ODU2ODI5OTkyLjE2NzA2ODcwMDE.*_ga_XQVN5K27XX*MTY3MDY4NzAwMC4xLjEuMTY3MDY4NzYyMy41Ni4wLjA.&_ga=2.153775102.712544994.1670687001-856829992.1670687001";
-               // var url = textBox1.Text; //"https://www.nhatot.com/mua-ban-nha-dat-quan-8-tp-ho-chi-minh/98893753.htm?_gl=1*1apb9n6*_ga*NTM0NDM0NDYzLjE2NzA2OTM4ODI.*_ga_XQVN5K27XX*MTY3MDY5Mzg4MS4xLjAuMTY3MDY5Mzg4MS42MC4wLjA.&_ga=2.28176127.2128205471.1670693882-534434463.1670693882";
-
-                //var url = "https://www.chotot.com/mua-ban-dien-thoai-thi-xa-tan-uyen-binh-duong/100554829.htm";
+                 //  var url = "https://xe.chotot.com/mua-ban-xe-tai-xe-ben-huyen-quoc-oai-ha-noi/101909944.htm?ctfp=3c15e79f-de73-41fa-ae40-4d7c8bb14ae7&_gl=1*1l0mli7*_ga*ODU2ODI5OTkyLjE2NzA2ODcwMDE.*_ga_XQVN5K27XX*MTY3MDY4NzAwMC4xLjEuMTY3MDY4NzYyMy41Ni4wLjA.&_ga=2.153775102.712544994.1670687001-856829992.1670687001";
+                 // var url = textBox1.Text; //"https://www.nhatot.com/mua-ban-nha-dat-quan-8-tp-ho-chi-minh/98893753.htm?_gl=1*1apb9n6*_ga*NTM0NDM0NDYzLjE2NzA2OTM4ODI.*_ga_XQVN5K27XX*MTY3MDY5Mzg4MS4xLjAuMTY3MDY5Mzg4MS42MC4wLjA.&_ga=2.28176127.2128205471.1670693882-534434463.1670693882";
+                 //var url = "https://www.chotot.com/mua-ban-dien-thoai-thi-xa-tan-uyen-binh-duong/100554829.htm";
                 try
                 {
                     driverC.Url = url;
@@ -87,7 +151,7 @@ namespace Crawler
                 {
                     RetrievePhoneNumberFirstapproch(driverC);
                 }
-                catch
+                catch(Exception ex)
                 {
                     RetrievePhoneNumber(driverC);
                 }
@@ -96,6 +160,7 @@ namespace Crawler
             {
                  driverC.FindElement(By.XPath("//button[.='Tin Tiếp']")).Click();
                 Thread.Sleep(3000);
+                textBox1.Text = driverC.Url;
             }
         }
         private void RetrievePhoneNumberFirstapproch(IWebDriver driverC)
@@ -139,18 +204,19 @@ namespace Crawler
             }
         }
         private void RegisterPhone(string phone)
-        { 
+        {
             if (!lst.Contains(phone))
             {
                 lst.Add(phone);
-                File.WriteAllText(pth, File.ReadAllText(pth) + Environment.NewLine + phone);
+                File.AppendAllText(pth, phone + Environment.NewLine);
+
+                // File.WriteAllText(pth, File.ReadAllText(pth) + Environment.NewLine + phone);
                 if (lst.Count == MaxTimes)
                 {
                     Is50000 = true;
-                    //this.Close();
-                   // Process.Kill.("");
                 }
             }
+            lblCount.Text = lst.Count.ToString();
 
         }
         private void RetrievePhoneNumber(IWebDriver driverC)
@@ -200,6 +266,7 @@ namespace Crawler
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            CheckForIllegalCrossThreadCalls = false;
             if (!Directory.Exists(@"C:\chotot"))
             {
                 Directory.CreateDirectory(@"C:\chotot");
@@ -207,6 +274,35 @@ namespace Crawler
             if (!File.Exists(pth))
             {
                 File.Create(pth);
+            }
+            lst = File.ReadAllLines(pth).ToList<string>();
+            lblCount.Text = lst.Count.ToString();
+            txtData.Text = pth;
+            txtDriver.Text = DriverPath;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start( Path.GetDirectoryName(pth));
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                cdr.Quit();
+            }
+            catch { }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+             
+            openFileDialog1.Filter = "Chrome Driver file (*.exe)|*.exe|All files (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtDriver.Text =openFileDialog1.FileName;
             }
         }
     }
